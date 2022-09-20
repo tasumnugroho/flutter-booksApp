d57c1ee7de661e2ec63d62f1a3282fcef68d85b6
tasumnugroho <nugroho.gatot04@gmail.com>
2022-09-20 23:22:30 +0700

    import project

---
+++ lib/views/book_list_page.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+import 'package:book_apps_nugro/models/book_list_response.dart';
+import 'package:book_apps_nugro/views/detail_book_page.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BookListPage extends StatefulWidget {
+  const BookListPage({Key? key}) : super(key: key);
+
+  @override
+  State<BookListPage> createState() => _BookListPageState();
+}
+
+class _BookListPageState extends State<BookListPage> {
+  BookListResponse? bookList;
+  fetchBookApi() async {
+    var url = Uri.parse('https://api.itbook.store/1.0/new');
+    print("test1 fetch");
+    try {
+      var response = await http.get(url);
+      print("test2 fetch");
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final jsonBookList = jsonDecode(response.body);
+        bookList = BookListResponse.fromJson(jsonBookList);
+        setState(() {});
+      }
+      print("$fetchBookApi");
+    } catch (e, s) {
+      print(e);
+      print(s);
+    }
+    // var response = await http.get(url);
+
+    // print(await http.read(Uri.https('example.com', 'foobar.txt')));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchBookApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("test1 build");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Book Catalogue"),
+      ),
+      body: Container(
+        child: bookList == null
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: bookList!.books!.length,
+                itemBuilder: (context, index) {
+                  final currentBook = bookList!.books![index];
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => DetailBookPage(
+                            isbn: currentBook.isbn13!,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      children: [
+                        Image.network(
+                          currentBook.image!,
+                          height: 100,
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(currentBook.title!),
+                                Text(currentBook.subtitle!),
+                                Align(
+                                  alignment: Alignment.topRight,
+                                  child: Text(currentBook.price!),
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+      ),
+    );
+    print("test2 build");
+  }
+  // print("book value $bookList");
+}
---
+++ lib/views/detail_book_page.dart
@@ -0,0 +1,160 @@
+import 'dart:convert';
+import 'package:book_apps_nugro/models/book_detail_response.dart';
+import 'package:book_apps_nugro/views/image_view_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DetailBookPage extends StatefulWidget {
+  const DetailBookPage({
+    Key? key,
+    required this.isbn,
+  }) : super(key: key);
+  final String isbn;
+  @override
+  State<DetailBookPage> createState() => _DetailBookPageState();
+}
+
+class _DetailBookPageState extends State<DetailBookPage> {
+  BookDetailResponse? detailBook;
+  fetchDetailBookApi() async {
+    print(widget.isbn);
+    var url = Uri.parse('https://api.itbook.store/1.0/books/${widget.isbn}');
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final jsonDetail = jsonDecode(response.body);
+      detailBook = BookDetailResponse.fromJson(jsonDetail);
+      setState(() {});
+    }
+
+    // print(await http.read(Uri.https('example.com', 'foobar.txt')));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchDetailBookApi();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail"),
+      ),
+      body: detailBook == null
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  ImageViewScreen(imageUrl: detailBook!.image!),
+                            ),
+                          );
+                        },
+                        child: Image.network(
+                          detailBook!.image!,
+                          height: 150,
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                detailBook!.title!,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                detailBook!.authors!,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                children: List.generate(
+                                  5,
+                                  (index) => Icon(
+                                    Icons.star,
+                                    color:
+                                        index < int.parse(detailBook!.rating!)
+                                            ? Colors.yellow
+                                            : Colors.grey,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                detailBook!.subtitle!,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Text(
+                                detailBook!.price!,
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.green,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            // fixedSize: Size(double.infinity, 50)
+                            ),
+                        onPressed: () {},
+                        child: Text("BUY")),
+                  ),
+                  SizedBox(height: 20),
+                  Text(detailBook!.desc!),
+                  SizedBox(height: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text("Year: " + detailBook!.year!),
+                      Text("ISBN" + detailBook!.isbn13!),
+                      Text(detailBook!.pages! + " Page"),
+                      Text("Publisher: " + detailBook!.publisher!),
+                      Text("Language: " + detailBook!.language!),
+
+                      // Text("Rating: " + detailBook!.rating!),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+    );
+  }
+}
